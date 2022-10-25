73fb366434d6413bdf7ff9a6422539d87ba8b321
mativ <selandermathias00@gmail.com>
2022-10-25 08:35:56 +0200

    point og køb

--- static/index.js
+++ static/index.js
@@ -1,1 +1,19 @@
 console.log("din mor");
+var point = 0;
+var kurvList = [];
+
+function plusPoint(n)
+{
+    point += n;
+    document.getElementById("points").innerHTML = "Points: " + point;
+}
+
+function addKurv(str, n)
+{
+    if(n <= point)
+    {
+        kurvList.push(str);
+        document.getElementById("kurvList").innerHTML = kurvList;
+        plusPoint(-n);
+    }
+}
