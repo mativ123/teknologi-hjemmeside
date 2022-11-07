3d31cb5714f4f1803b371878ad33384976785769
mativ <selandermathias00@gmail.com>
2022-11-07 13:23:33 +0100

    alt muligt

--- static/index.js
+++ static/index.js
@@ -1,1 +1,32 @@
-console.log("din mor");
+var point = 0;
+var tPoint = point;
+var kurvList = [];
+
+function plusPoint(n)
+{
+    point += n;
+}
+
+function addKurv(str, n)
+{
+    if(n <= point)
+    {
+        kurvList.push(str);
+        document.getElementById("kurvList").innerHTML = kurvList;
+        plusPoint(-n);
+    }
+}
+
+function main()
+{
+
+    if(tPoint != point)
+    {
+        document.getElementById("points").innerHTML = "Points: " + point;
+        tPoint = point;
+    }
+
+    setTimeout(main, 10);
+}
+
+main();
